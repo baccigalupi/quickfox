e8d17588274d0dea3235b97b3cf7fb1fba1d324d
Kane Baccigalupi <baccigalupi@gmail.com>
2014-10-31 15:50:00 -0700

    Update lazydog.rb
    Looks good!

--- lazydog.rb
+++ lazydog.rb
@@ -1,4 +1,4 @@
-
+# Don't commit the .DS_Store. Also since you are doing it in one line, you don't need the variable. Just puts it directly
 quick_fox = "The quick brown fox jumps over the lazy dog.".gsub("e", "3").upcase
 
 puts quick_fox
